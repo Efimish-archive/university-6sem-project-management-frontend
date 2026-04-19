0f1c370ef84cac47b638369721beae22c12a1ed6
Efim Ishenin <efim.ishenin@gmail.com>
2026-04-19 17:37:13 +0700

    feat: add funny login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+import { LogIn, Car, Van } from "lucide-react";
+
 function App() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex flex-col gap-4 bg-amber-200 p-4">
-        <h1 className="text-2xl font-extrabold text-amber-700">
+    <div className="flex h-screen w-screen items-center justify-center bg-amber-100">
+      <div className="flex flex-col items-center gap-2 rounded-lg border-2 border-yellow-500 bg-yellow-400 px-6 py-2">
+        <h1 className="text-2xl font-extrabold text-amber-700 text-shadow-amber-200 text-shadow-lg">
           Войдите в систему
         </h1>
-        <input className="bg-amber-400" type="text" />
-        <input className="bg-amber-400" type="text" />
+        <input
+          className="rounded-md border-2 border-blue-500 bg-blue-400 px-1 font-bold text-neutral-50 text-shadow-black text-shadow-xs placeholder:text-neutral-200"
+          type="text"
+          placeholder="Логин"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <input
+          className="rounded-md border-2 border-blue-500 bg-blue-400 px-1 font-bold text-neutral-50 text-shadow-black text-shadow-xs placeholder:text-neutral-200"
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="flex gap-2">
+          <Car />
+          <button className="flex w-fit cursor-pointer gap-2 rounded-md border-2 border-green-500 bg-green-400 px-1 font-bold text-neutral-50 text-shadow-black text-shadow-xs">
+            <p>Войти</p>
+            <LogIn />
+          </button>
+          <Van />
+        </div>
       </div>
     </div>
   );
