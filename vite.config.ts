f8ec116a3ed7ce15dbdfca5a3085760ff7373a11
Efim Ishenin <efim.ishenin@gmail.com>
2026-04-19 18:44:20 +0700

    refactor: simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("src", import.meta.url)),
-      },
-    ],
+    tsconfigPaths: true,
   },
   plugins: [react(), tailwindcss()],
 });
