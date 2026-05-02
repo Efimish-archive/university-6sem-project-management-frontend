6075e09e3719ca426ca787b6dad984c442100721
Efim Ishenin <efim.ishenin@gmail.com>
2026-05-02 14:10:06 +0700

    feat: make number recognition work

--- src/api/types.gen.ts
+++ src/api/types.gen.ts
@@ -6,6 +6,7 @@ export type ClientOptions = {
 
 export type Error = {
     error: string;
+    code: 100 | 101 | 102 | 103 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 300 | 301 | 302 | 303 | 304 | 307 | 308 | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 420 | 421 | 422 | 423 | 424 | 425 | 426 | 428 | 429 | 431 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511;
 };
 
 export type PostAuthLoginData = {
@@ -238,7 +239,7 @@ export type PostNumbersCheckResponses = {
      * Response for status 200
      */
     200: {
-        message: string;
+        number: string;
     };
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,97 @@
+import {
+  useRef,
+  useState,
+  type ChangeEvent,
+  type SubmitEventHandler,
+} from "react";
 import { useToken } from "@/useToken";
+
+import { postNumbersCheck } from "@/api";
+import clsx from "clsx";
 
 function Home() {
   useToken();
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [resultNumber, setResultNumber] = useState<string | null>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(event.target.files![0]); // Access the first file in the FileList
+
+    const reader = new FileReader();
+    reader.addEventListener("load", (ev) => {
+      if (!imageRef.current) return;
+      imageRef.current.src = ev.target?.result as string;
+    });
+
+    reader.readAsDataURL(event.target.files![0]);
+  };
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    if (!selectedFile) return;
+
+    const { data } = await postNumbersCheck({
+      body: {
+        file: selectedFile,
+      },
+      bodySerializer: (body) => {
+        const formData = new FormData();
+        // @ts-expect-error do not expect the error here
+        formData.set("file", body.file);
+        return formData;
+      },
+      headers: {
+        "Content-Type": null,
+      },
+    });
+
+    // TODO: обработать
+    if (typeof data === "undefined") return;
+    setResultNumber(data.number);
+  };
+
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-amber-100">
-      <div className="flex flex-col items-center gap-2 rounded-lg border-2 border-yellow-500 bg-yellow-400 px-6 py-2">
+      <div className="flex flex-col items-center justify-center gap-2 rounded-lg border-2 border-yellow-500 bg-yellow-400 px-6 py-2">
         <h1 className="text-2xl font-extrabold text-amber-700 text-shadow-amber-200 text-shadow-lg">
           Добро пожаловать!
         </h1>
+        <form
+          className="flex flex-col items-center justify-center gap-2"
+          onSubmit={handleSubmit}
+        >
+          <label>
+            <span className="cursor-pointer rounded-md border-2 border-blue-500 bg-blue-400 px-1 font-bold text-neutral-50 text-shadow-black text-shadow-xs">
+              Выбрать картинку с номером
+            </span>
+            <input
+              className="hidden"
+              type="file"
+              accept="image/png, image/jpeg, image/webp"
+              onChange={handleFileChange}
+            />
+          </label>
+          <img ref={imageRef} src="" alt="" />
+          <button
+            className={clsx(
+              "flex w-fit cursor-pointer gap-2 rounded-md border-2 border-green-500 bg-green-400 px-1 font-bold text-neutral-50 text-shadow-black text-shadow-xs",
+              { hidden: selectedFile === null },
+            )}
+            type="submit"
+            disabled={selectedFile === null}
+          >
+            Узнать номер
+          </button>
+        </form>
+        <div
+          className={clsx(
+            "rounded-sm border-2 border-neutral-900 bg-neutral-50 px-1",
+            { hidden: resultNumber === null },
+          )}
+        >
+          {resultNumber}
+        </div>
       </div>
     </div>
   );
