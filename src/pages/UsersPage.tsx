04247f794227aab4d89bb10f6d383e45d9c40aa9
Efim Ishenin <efim.ishenin@gmail.com>
2026-05-14 16:35:28 +0700

    add chadcn table and datatable, add tanstack table

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useSeoMeta } from "@unhead/react";
 import { getUsers } from "@/api";
 import { useToken } from "@/useToken";
+import { DataTable } from "@/components/DataTable";
 
 function UsersPage() {
   useToken();
@@ -18,34 +19,37 @@ function UsersPage() {
       }),
   });
 
+  if (query.isPending) {
+    return <div>Loading</div>;
+  }
+
+  if (query.isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <div className="flex max-h-full justify-center overflow-scroll">
-      <table>
-        <thead>
-          <tr>
-            <th className="border-2 border-amber-900 px-2">ID</th>
-            <th className="border-2 border-amber-900 px-2">Фамилия</th>
-            <th className="border-2 border-amber-900 px-2">Имя</th>
-            <th className="border-2 border-amber-900 px-2">Отчество</th>
-          </tr>
-        </thead>
-        <tbody>
-          {query.data?.data?.map((user) => (
-            <tr>
-              <td className="border-2 border-amber-900 px-2">{user.id}</td>
-              <td className="border-2 border-amber-900 px-2">
-                {user.lastName}
-              </td>
-              <td className="border-2 border-amber-900 px-2">
-                {user.firstName}
-              </td>
-              <td className="border-2 border-amber-900 px-2">
-                {user.middleName}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable
+        columns={[
+          {
+            accessorKey: "id",
+            header: "ID",
+          },
+          {
+            accessorKey: "lastName",
+            header: "Фамилия",
+          },
+          {
+            accessorKey: "firstName",
+            header: "Имя",
+          },
+          {
+            accessorKey: "middleName",
+            header: "Отчество",
+          },
+        ]}
+        data={query.data.data!}
+      />
     </div>
   );
 }
